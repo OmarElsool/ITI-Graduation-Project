a30137c5aa593633c618c0854c4bb7ce91d2e10c
OmarElsool <omarbaka1999@gmail.com>
2023-03-07 01:34:38 +0200

    mansion uncompleted state

---
+++ Controllers/MansionController.cs
@@ -0,0 +1,23 @@
+﻿using Airbnb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Airbnb.Controllers
+{
+    public class MansionController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public MansionController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var mansionState = db.Mansions.Include(m => m.Reservations).ThenInclude(m => m.User).Where(m => m.UserId == userId).ToList();
+            return View(mansionState);
+        }
+    }
+}
---
+++ wwwroot/js/mansion-details.js
@@ -0,0 +1,12 @@
+//$(function(){
+//    var resCard = $('.reserve-card-details');
+//    var resCardTop = resCard.offset().top;
+//    var lastSectionDetailsBottom = $('.last-section-details-bottom').offset().top;
+//    var win = $(window);
+//    win.scroll(function(e){
+//        var scrollTop = win.scrollTop();
+//        if(scrollTop >= resCardTop && scrollTop <= lastSectionDetailsBottom - 500){
+//            resCard.css({position:'absolute',top:scrollTop})
+//        }
+//    });
+//});
